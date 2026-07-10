185f2097ef0c9bb34894628da4e8392bc97ced26
noahji <jizc@51mydao.com>
2026-07-10 14:12:12 +0800

    build: replace legacy scaffold with library toolchain

--- stylelint.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('stylelint').Config} */
-export default {
-  extends: ['stylelint-config-standard-less'],
-  rules: {
-    'selector-class-pattern': null,
-    'less/no-duplicate-variables': null,
-    'declaration-property-value-no-unknown': null,
-  },
-};
